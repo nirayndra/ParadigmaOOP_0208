390940ed89d8c11b153f81a5d3311e8e2e907b1e
nirayndra <faadiajasmine@gmail.com>
2026-04-20 17:37:03 +0700

    melengkapi output member ke empat

--- introductionOOP.cpp
+++ introductionOOP.cpp
@@ -8,5 +8,8 @@ public:
     float nilai;
 
     void printData(){
+        cout << "NIM: " << nim << endl;
+        cout << "Nama: " << nama << endl;
+        cout << "Nilai: " << nilai << endl;
     }
 };
